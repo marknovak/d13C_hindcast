0c2abf86a245eab4d3fe34d3a6093e15312d3b98
Mark Novak <mnovak.m@gmail.com>
2021-02-15 17:44:08 -0800

    Update in response to 1st review

--- Code/DataProcessing.R
+++ Code/DataProcessing.R
@@ -1,19 +1,30 @@
 #######################################################
 # Script to pre-process the data prior to model-fitting
 #######################################################
-rm(list=ls())
+rm(list = ls())
 
-odat<-read.csv('../Data/Taylor_FormalinFixation.csv',header=T,stringsAsFactors=FALSE)
+odat <-
+  read.csv(
+    '../Data/Taylor_FormalinFixation.csv',
+    header = T,
+    stringsAsFactors = FALSE
+  )
 
 # Convert storage years to days
-odat$Storage_days <- 365*odat$Storage_years
+odat$Storage_days <- 365 * odat$Storage_years
 
 # Combine to total preservation
 odat$Preservation_days <- odat$Fixation_days + odat$Storage_days
 
+
+# Range of variation for in-house standards
+range(c(odat$d13C_sd_Run1, odat$d13C_sd_Run2), na.rm=TRUE)
+range(c(odat$d15N_sd_Run1, odat$d15N_sd_Run2), na.rm=TRUE)
+
+
 # Due to an isotope analyzer failure, many of the 'experimental' (i.e. 'lab-processed')
-# specimen sample had to be run a second time.   Only the d15N readings were affected, 
-# and for some specimens there was not enough material remaining to permit a second run.  
+# specimen sample had to be run a second time.   Only the d15N readings were affected,
+# and for some specimens there was not enough material remaining to permit a second run.
 # Therefore, for the 'experimental' specimens, we:
 # (i) take the average d13C of both Run 1 and 2, using only Run 1 when there is no Run 2
 # (ii) use Run 2 for d15N when both Run 1 and 2 were available
@@ -24,12 +35,18 @@ odat$d15N <- odat$d15N_sd <- odat$d15N_sd_n <- NA
 
 # Which have both Run 1 and Run 2 d13C values?  Take their average.
 # Those that have only a Run 1 value will use that value (because na.rm=TRUE)
-odat$d13C <- apply(cbind(odat$d13C_Run1, odat$d13C_Run2),1,mean, na.rm=TRUE)
-odat$d13C_sd <- apply(cbind(odat$d13C_sd_Run1, odat$d13C_sd_Run2),1,mean, na.rm=TRUE)
-odat$d13C_sd_n <- apply(cbind(odat$d13C_sd_n_Run1, odat$d13C_sd_nRun2),1,mean, na.rm=TRUE)
+odat$d13C <-
+  apply(cbind(odat$d13C_Run1, odat$d13C_Run2), 1, mean, na.rm = TRUE)
+odat$d13C_sd <-
+  apply(cbind(odat$d13C_sd_Run1, odat$d13C_sd_Run2), 1, mean, na.rm = TRUE)
+odat$d13C_sd_n <-
+  apply(cbind(odat$d13C_sd_n_Run1, odat$d13C_sd_nRun2),
+        1,
+        mean,
+        na.rm = TRUE)
 
 # Identify 'experimental' specimens
-expmt <- odat$Source=='Experiment'
+expmt <- odat$Source == 'Experiment'
 
 # For 'field' specimens, use Run 1 d15N
 odat$d15N[!expmt] <- odat$d15N_Run1[!expmt]
@@ -52,21 +69,38 @@ odat$d15N_sd_n[sel] <- odat$d15N_sd_n_Run1[sel]
 # This many specimens did not have a d15N value in either Run 1 or Run 2
 sum(is.na(odat$d15N))
 
+
 # Select columns to keep
-dat <- odat[,c('Collector_Number','Source',
-               'Fixation_days','Storage_days','Preservation_days',
-               'd13C','d13C_sd','d13C_sd_n',
-               'd15N','d15N_sd','d15N_sd_n')]
-               
+dat <- odat[, c(
+  'Collector_Number',
+  'Source',
+  'Fixation_days',
+  'Storage_days',
+  'Preservation_days',
+  'd13C',
+  'd13C_sd',
+  'd13C_sd_n',
+  'd15N',
+  'd15N_sd',
+  'd15N_sd_n'
+)]
+
 # Abbreviate predictors
-colnames(dat)<-sub('Collector_Number','SpecimenID',colnames(dat))
-colnames(dat)<-sub('Fixation_days','Fix',colnames(dat))
-colnames(dat)<-sub('Storage_days','EStor',colnames(dat))
-colnames(dat)<-sub('Preservation_days','Pres',colnames(dat))
+colnames(dat) <-
+  sub('Collector_Number', 'SpecimenID', colnames(dat))
+colnames(dat) <- sub('Fixation_days', 'Fix', colnames(dat))
+colnames(dat) <- sub('Storage_days', 'EStor', colnames(dat))
+colnames(dat) <- sub('Preservation_days', 'Pres', colnames(dat))
 
 # Export processed data for subsequent importing
-save(file='../Results/ProcessedData.Rdata',dat)
+save(file = '../Results/ProcessedData.Rdata', dat)
 
+
+# Reviewer request:  What was mean difference between runs?
+mean(odat$d13C_Run1 - odat$d13C_Run2, na.rm=TRUE)
+sd(odat$d13C_Run1 - odat$d13C_Run2, na.rm=TRUE)
+mean(odat$d15N_Run1 - odat$d15N_Run2, na.rm=TRUE)
+sd(odat$d15N_Run1 - odat$d15N_Run2, na.rm=TRUE)
 #######################################################
 #######################################################
 #######################################################
--- Code/ModelFitting.R
+++ Code/ModelFitting.R
@@ -1,7 +1,7 @@
 ################################################
 # Fit and compare nonlinear mixed-effects models
 ################################################
-rm(list=ls())
+rm(list = ls())
 
 library(nlme)
 library(lme4)
@@ -15,13 +15,13 @@ library(bbmle) # for *ICtab()
 ######################
 # Import prepared data
 ######################
-load(file='../Results/ProcessedData.Rdata')
+load(file = '../Results/ProcessedData.Rdata')
 
 #############################
 # Mean initial specimen value
 #############################
-IestC <- mean(dat$d13C[dat$EStor==0], na.rm = T)
-IestN <- mean(dat$d15N[dat$EStor==0], na.rm = T)
+IestC <- mean(dat$d13C[dat$EStor == 0], na.rm = T)
+IestN <- mean(dat$d15N[dat$EStor == 0], na.rm = T)
 
 #########################################################################################
 #########################################################################################
@@ -33,61 +33,69 @@ IestN <- mean(dat$d15N[dat$EStor==0], na.rm = T)
 #~~~~~~~~~~~~~~~~~~~
 #~~~~ Nitrogen ~~~~~
 #~~~~~~~~~~~~~~~~~~~
-fit0N<-nlme(d15N ~ I,
-            fixed=I~1,
-            random=I~1|SpecimenID,
-            start=c(I=IestN),
-            data=dat, verbose=F,
-            na.action = na.omit)
+fit0N <- nlme(
+  d15N ~ I,
+  fixed = I ~ 1,
+  random = I ~ 1 | SpecimenID,
+  start = c(I = IestN),
+  data = dat,
+  verbose = F,
+  na.action = na.omit
+)
 summary(fit0N)
 
 #~~~~~~~~~~~~~~~~~
 #~~~~ Carbon ~~~~~
 #~~~~~~~~~~~~~~~~~
-fit0C<-nlme(d13C ~ I,
-           fixed=I~1,
-           random=I~1|SpecimenID,
-           start=c(I=IestC),
-           data=dat, verbose=F)
+fit0C <- nlme(
+  d13C ~ I,
+  fixed = I ~ 1,
+  random = I ~ 1 | SpecimenID,
+  start = c(I = IestC),
+  data = dat,
+  verbose = F
+)
 summary(fit0C)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~  Repeat null model using lme4:nlmer ~~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mod0 <- function(I){ I }
+mod0 <- function(I) {
+  I
+}
 mod0gr <- deriv(body(mod0)[[2]],
                 namevec = c('I'),
-                function.arg=mod0)
+                function.arg = mod0)
 
 #~~~~ Nitrogen ~~~~~
-starts <- apply(coef(fit0N),2,mean)
+starts <- apply(coef(fit0N), 2, mean)
 
 fit0Nr <- nlmer(
   # Response
-  d15N ~ 
+  d15N ~
     # Fixed effects
-    mod0gr(I) ~ 
+    mod0gr(I) ~
     # Random effects
-    (I | SpecimenID), 
+    (I | SpecimenID),
   # Data
-  data = dat, 
+  data = dat,
   start = starts,
-  na.action = na.omit)
+  na.action = na.omit
+)
 
 summary(fit0Nr)
 
 #~~~~ Carbon ~~~~~
-starts <- apply(coef(fit0C),2,mean)
+starts <- apply(coef(fit0C), 2, mean)
 
-fit0Cr <- nlmer(
-  # Response
-  d13C ~ 
+fit0Cr <- nlmer(# Response
+  d13C ~
     # Fixed effects
-    mod0gr(I) ~ 
+    mod0gr(I) ~
     # Random effects
-    (I | SpecimenID), 
+    (I | SpecimenID),
   # Data
-  data = dat, 
+  data = dat,
   start = starts)
 
 summary(fit0Cr)
@@ -100,26 +108,32 @@ summary(fit0Cr)
 #~~~~~~~~~~~~~~~~~~~
 #~~~~ Nitrogen ~~~~~
 #~~~~~~~~~~~~~~~~~~~
-starts <- c(I=IestN, A=-0.03,l1=0.001)
+starts <- c(I = IestN, A = -0.03, l1 = 0.001)
 
-fit1N<-nlme(d15N ~ I + A*(1-exp(l1*Pres)),
-            fixed=I+A+l1~1,
-            random=I~1|SpecimenID,
-            start=starts,
-            data=dat, verbose=F,
-            na.action = na.omit)
+fit1N <- nlme(
+  d15N ~ I + A * (1 - exp(l1 * Pres)),
+  fixed = I + A + l1 ~ 1,
+  random = I ~ 1 | SpecimenID,
+  start = starts,
+  data = dat,
+  verbose = F,
+  na.action = na.omit
+)
 summary(fit1N)
 
 #~~~~~~~~~~~~~~~~~
 #~~~~ Carbon ~~~~~
 #~~~~~~~~~~~~~~~~~
-starts <- c(I=IestC,A=-0.9,l1=-.001)
+starts <- c(I = IestC, A = -0.9, l1 = -.001)
 
-fit1C<-nlme(d13C ~ I + A*(1-exp(l1*Pres)),
-            fixed=I+A+l1~1,
-            random=I~1|SpecimenID,
-            start=starts,
-            data=dat, verbose=F)
+fit1C <- nlme(
+  d13C ~ I + A * (1 - exp(l1 * Pres)),
+  fixed = I + A + l1 ~ 1,
+  random = I ~ 1 | SpecimenID,
+  start = starts,
+  data = dat,
+  verbose = F
+)
 summary(fit1C)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -127,29 +141,31 @@ summary(fit1C)
 # ~~~~~~      in order to get better estimates of  ~~~~~~~~~
 # ~~~~~~                 uncertanties              ~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mod1 <- function(Pres, I, A, l1){
-  I + A*(1-exp(l1*Pres))
+mod1 <- function(Pres, I, A, l1) {
+  I + A * (1 - exp(l1 * Pres))
 }
 mod1gr <- deriv(body(mod1)[[2]],
-                namevec = c('I','A','l1'),
-                function.arg=mod1)
+                namevec = c('I', 'A', 'l1'),
+                function.arg = mod1)
 
 #~~~~ Carbon ~~~~~
-starts <- apply(coef(fit1C),2,mean)
-starts <- list(nlpars= apply(coef(fit1C),2,mean), theta=vcov(fit1C))
+starts <- apply(coef(fit1C), 2, mean)
+starts <-
+  list(nlpars = apply(coef(fit1C), 2, mean), theta = vcov(fit1C))
 
 fit1Cr <- nlmer(
   # Response
-  d13C ~ 
+  d13C ~
     # Fixed effects
-    mod1gr(Pres=Pres, I, A, l1) ~ 
+    mod1gr(Pres = Pres, I, A, l1) ~
     # Random effects
-    (I | SpecimenID), 
+    (I | SpecimenID),
   # Data
-  data = dat, 
+  data = dat,
   start = starts,
-  verbose=F,
-  control=nlmerControl(tolPwrss=1E-4))
+  verbose = F,
+  control = nlmerControl(tolPwrss = 1E-4)
+)
 
 summary(fit1Cr)
 
@@ -163,28 +179,33 @@ summary(fit1Cr)
 #~~~~~~~~~~~~~~~~~~~
 #~~~~ Nitrogen ~~~~~
 #~~~~~~~~~~~~~~~~~~~
-starts <- c(apply(coef(fit1N),2,mean),l2=0.001)
+starts <- c(apply(coef(fit1N), 2, mean), l2 = 0.001)
 
-fit2N<-nlme(d15N ~ I + A*(1-exp(l1*Fix)*exp(l2*EStor)),   
-            fixed=I+A+l1+l2~1,
-            random=I~1|SpecimenID,
-            start=starts,
-            data=dat, 
-            verbose=F,
-            control = nlmeControl(pnlsTol = 0.07, msVerbose = TRUE),
-            na.action = na.omit)
+fit2N <- nlme(
+  d15N ~ I + A * (1 - exp(l1 * Fix) * exp(l2 * EStor)),
+  fixed = I + A + l1 + l2 ~ 1,
+  random = I ~ 1 | SpecimenID,
+  start = starts,
+  data = dat,
+  verbose = F,
+  control = nlmeControl(pnlsTol = 0.07, msVerbose = TRUE),
+  na.action = na.omit
+)
 summary(fit2N)
 
 #~~~~~~~~~~~~~~~~~
 #~~~~ Carbon ~~~~~
 #~~~~~~~~~~~~~~~~~
-starts <- c(apply(coef(fit1C),2,mean),l2=-0.001)
+starts <- c(apply(coef(fit1C), 2, mean), l2 = -0.001)
 
-fit2C<-nlme(d13C ~ I + A*(1-exp(l1*Fix)*exp(l2*EStor)),
-           fixed=I+A+l1+l2~1,
-           random=I~1|SpecimenID,
-           start=starts,
-           data=dat, verbose=F)
+fit2C <- nlme(
+  d13C ~ I + A * (1 - exp(l1 * Fix) * exp(l2 * EStor)),
+  fixed = I + A + l1 + l2 ~ 1,
+  random = I ~ 1 | SpecimenID,
+  start = starts,
+  data = dat,
+  verbose = F
+)
 summary(fit2C)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -192,26 +213,27 @@ summary(fit2C)
 # ~~~~~~     in order to get better estimates of  ~~~~~~~~~
 # ~~~~~~                uncertanties              ~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mod2 <- function(Fix, EStor, I, A, l1, l2){
-    I + A*(1-exp(l1*Fix)*exp(l2*EStor)) # equivalent to exp( l1*Fix + l2*EStor )
+mod2 <- function(Fix, EStor, I, A, l1, l2) {
+  I + A * (1 - exp(l1 * Fix) * exp(l2 * EStor)) # equivalent to exp( l1*Fix + l2*EStor )
 }
 mod2gr <- deriv(body(mod2)[[2]],
-                namevec = c('I','A','l1','l2'),
-                function.arg=mod2)
+                namevec = c('I', 'A', 'l1', 'l2'),
+                function.arg = mod2)
 
 #~~~~ Carbon ~~~~~
-starts <- apply(coef(fit2C),2,mean)
+starts <- apply(coef(fit2C), 2, mean)
 
 fit2Cr <- nlmer(
   # Response
-  d13C ~ 
-  # Fixed effects
-  mod2gr(Fix=Fix, EStor=EStor, I, A, l1, l2) ~ 
-  # Random effects
-  (I | SpecimenID), 
+  d13C ~
+    # Fixed effects
+    mod2gr(Fix = Fix, EStor = EStor, I, A, l1, l2) ~
+    # Random effects
+    (I | SpecimenID),
   # Data
-  data = dat, 
-  start = starts)
+  data = dat,
+  start = starts
+)
 
 summary(fit2Cr)
 
@@ -224,33 +246,90 @@ summary(fit2Cr)
 #~~~~~~~~~~~~~~~~~~~
 #~~~~ Nitrogen ~~~~~
 #~~~~~~~~~~~~~~~~~~~
-bbmle::BICtab(fit0N,fit1N,fit2N, logLik=TRUE, base=TRUE,sort=FALSE, weights=TRUE)
-bbmle::AICctab(fit0N,fit1N,fit2N, logLik=TRUE, base=TRUE,sort=FALSE, weights=TRUE)
+bbmle::BICtab(
+  fit0N,
+  fit1N,
+  fit2N,
+  logLik = TRUE,
+  base = TRUE,
+  sort = FALSE,
+  weights = TRUE
+)
+bbmle::AICctab(
+  fit0N,
+  fit1N,
+  fit2N,
+  logLik = TRUE,
+  base = TRUE,
+  sort = FALSE,
+  weights = TRUE
+)
 
 #~~~~~~~~~~~~~~~~~
 #~~~~ Carbon ~~~~~
 #~~~~~~~~~~~~~~~~~
-bbmle::BICtab(fit0C,fit1C,fit2C, logLik=TRUE, base=TRUE,sort=FALSE, weights=TRUE)
-bbmle::AICctab(fit0C,fit1C,fit2C, logLik=TRUE, base=TRUE,sort=FALSE, weights=TRUE)
+bbmle::BICtab(
+  fit0C,
+  fit1C,
+  fit2C,
+  logLik = TRUE,
+  base = TRUE,
+  sort = FALSE,
+  weights = TRUE
+)
+bbmle::AICctab(
+  fit0C,
+  fit1C,
+  fit2C,
+  logLik = TRUE,
+  base = TRUE,
+  sort = FALSE,
+  weights = TRUE
+)
 
-bbmle::BICtab(fit0Cr,fit1Cr,fit2Cr, logLik=TRUE, base=TRUE,sort=FALSE, weights=TRUE)
-bbmle::AICctab(fit0Cr,fit1Cr,fit2Cr, logLik=TRUE, base=TRUE,sort=FALSE, weights=TRUE)
+bbmle::BICtab(
+  fit0Cr,
+  fit1Cr,
+  fit2Cr,
+  logLik = TRUE,
+  base = TRUE,
+  sort = FALSE,
+  weights = TRUE
+)
+bbmle::AICctab(
+  fit0Cr,
+  fit1Cr,
+  fit2Cr,
+  logLik = TRUE,
+  base = TRUE,
+  sort = FALSE,
+  weights = TRUE
+)
 
 ##############################################################################
 ##############################################################################
 # Save models and fits
-save(file='../Results/ModelFits.Rdata',
-     fit0N,fit1N,fit2N,
-     fit0C,fit1C,fit2C,
-     fit1Cr,fit2Cr,
-     mod1,mod1gr,
-     mod2,mod2gr)
+save(
+  file = '../Results/ModelFits.Rdata',
+  fit0N,
+  fit1N,
+  fit2N,
+  fit0C,
+  fit1C,
+  fit2C,
+  fit1Cr,
+  fit2Cr,
+  mod1,
+  mod1gr,
+  mod2,
+  mod2gr
+)
 
 #############################################################################
 #############################################################################
 # Comment on model-fitting methods:
 # lme4:nlmer producted tighter uncertainty bounds around point estimates
-#  Why?  Haven't bothered to find out, but it's the replacement for nmle 
+#  Why?  Haven't bothered to find out, but it's the replacement for nmle
 #  so perhaps there are indeed some improvements.
 #  It could also be because we're providing the gradient to nlmer.
 #  Note also the nlmer doesn't count degrees of freedom
@@ -258,8 +337,3 @@ save(file='../Results/ModelFits.Rdata',
 #############################################################################
 #############################################################################
 #############################################################################
-
-
-
-
-
